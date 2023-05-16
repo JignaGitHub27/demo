7945bd9edfdfefd6950ff85da0dfb2b311ffc8a4
Jigna <jignagajjar27@gmail.com>
2023-05-16 15:32:25 +0530

    index.php file generated

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>
+            Demo index file
+        </title>
+    </head>
+    <body>
+        <div style="border:1px solid black;text-align:center;font-size:20px;height:50px;">
+        <?php 
+            echo "hello world";
+        ?>
+        </div>
+        <div style="border:1px solid black;text-align:center;font-size:20px;height:50px;">
+        <?php 
+            echo "Test git example";
+        ?>
+        </div>
+    </body>
+</html>
