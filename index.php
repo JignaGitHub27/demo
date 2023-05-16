82e2679a0f486dd5783984b600ac410b530402f4
JignaGitHub27 <99484357+JignaGitHub27@users.noreply.github.com>
2023-05-16 15:34:13 +0530

    view git steps
    jigna/git - steps

--- index.php
+++ index.php
@@ -15,5 +15,8 @@
             echo "Test git example";
         ?>
         </div>
+        <div style="border:1px solid black;text-align:center;font-size:20px;height:50px;">
+        View Steps of git file into jigna/git steps  in word file
+        </div>
     </body>
 </html>
